61d54832d206d95efb99155d6824645ba7a756df
g00405841 <g00405841@atu.ie>
2024-10-01 16:54:32 +0000

    Exercise 5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 // Importing the Components
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 import Content from './components/Content';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -9,6 +11,14 @@ function App() {
   return (
     <div className="App">
       {/* Render the components */}
+      <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
+    </Router>
       <Header></Header>
       <Content></Content>
       <Footer></Footer>
