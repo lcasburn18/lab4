85d08a320d64160e6bb90dad597ddf06dc42c21c
g00405841 <g00405841@atu.ie>
2024-10-14 13:53:39 +0000

    Intial commit

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,8 +1,51 @@
-const Create= () => { 
-    return (
-        <h3>Hello from the Create component</h3>
-);
+// create.js
+
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [poster, setPoster] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Movie Title:", title);
+    console.log("Movie Year:", year);
+    console.log("Movie Poster:", poster);
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Add Movie Title: </label>
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Add Movie Year: </label>
+          <input type="text"
+            className="form-control"
+            value={year}
+            onChange={(e) => { setYear(e.target.value) }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Add Movie Poster: </label>
+          <input type="text"
+            className="form-control"
+            value={poster}
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />
+      </form>
+    </div>
+  );
 }
 
-// Export the Create Component
-  export default Create;
+export default Create;
--- src/components/Read.js
+++ src/components/Read.js
@@ -1,38 +1,26 @@
 // Imports
 import React from 'react';
 import Movies from './Movies';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-// Define the Read component
-const Read = () => {
-  // Define a constant array of movie data
-  const movieData = [
-    {
-      "Title": "Avengers: Infinity War", // Title of the movie
-      "Year": "2018", // Release year
-      "imdbID": "tt4154756", // IMDb ID for linking
-      "Type": "movie", // Type of media
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg" // URL of the movie poster
-    },
-    {
-      "Title": "Captain America: Civil War",
-      "Year": "2016",
-      "imdbID": "tt3498820",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-    },
-    {
-      "Title": "World War Z",
-      "Year": "2013",
-      "imdbID": "tt0816711",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-    }
-  ];
+function Read() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+      .then((response) => {
+        setMovies(response.data.movies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div>
       <h2>Movie List</h2> {/* Header for the movie list */}
-      <Movies movies={movieData} /> {/* Pass the movieData array to the Movies component as a prop */}
+      <Movies movies={movies} /> {/* Pass the movieData array to the Movies component as a prop */}
     </div>
   );
 };
